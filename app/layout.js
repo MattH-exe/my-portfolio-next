6d8ec0d64c8b98d191d5be432fea02460e013642
MattH-exe <78046933+MattH-exe@users.noreply.github.com>
2026-03-16 10:03:12 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 export const metadata = {
   title: "Hi, I'm Matt",
   description:
@@ -7,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <Analytics />
+      </body>
     </html>
   );
 }

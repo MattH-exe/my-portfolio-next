a884cd3e48f2740d365be3d33033868230f66db7
MattH-exe <78046933+MattH-exe@users.noreply.github.com>
2026-04-07 09:10:28 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/icon.png" sizes="any" />
+        <link rel="icon" href="/icon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
